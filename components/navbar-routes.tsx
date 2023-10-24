85a6d5e07a187c9d030f2d1260107a5d75890251
bobby lin <linzhangsheng23@gmail.com>
2023-10-24 10:07:59 +0800

    feat:add teacher mode

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -4,9 +4,11 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { LogOut } from 'lucide-react'
-import { UserButton } from '@clerk/nextjs'
+import { useAuth, UserButton } from '@clerk/nextjs'
+import { isTeacher } from '@/lib/teacher'
 
 export const NavbarRoutes = () => {
+  const { userId } = useAuth()
   const pathname = usePathname()
 
   const isTeacherPage = pathname.startsWith('/teacher')
@@ -22,6 +24,12 @@ export const NavbarRoutes = () => {
             <Button size="sm" variant="ghost">
               <LogOut className="mr-2 h-4 w-4" />
               Exit
+            </Button>
+          </Link>
+        ) : isTeacher(userId) ? (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher mode
             </Button>
           </Link>
         ) : null}
