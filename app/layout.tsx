2670bf17eb97b023f08d65d9e02a87daf6ab70ad
bobby lin <linzhangsheng23@gmail.com>
2023-10-24 13:57:08 +0800

    feat:add course ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/theme-provider'
+import { ToastProvider } from '@/components/toast-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange={false}
           >
+            <ToastProvider />
             {children}
           </ThemeProvider>
         </body>
