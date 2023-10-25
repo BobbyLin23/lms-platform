643af482688c8b4d51142589b6359f8a3a70295f
bobby lin <linzhangsheng23@gmail.com>
2023-10-25 13:16:28 +0800

    feat:add chapter edit api

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -85,3 +85,91 @@ export async function PATCH(
     console.log('[PATCH_COURSE_CHAPTER]', error)
   }
 }
+
+export async function DELETE(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      chapterId: string
+      courseId: string
+    }
+  }
+) {
+  try {
+    const { userId } = auth()
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    })
+
+    if (!ownCourse) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+    })
+
+    if (!chapter) {
+      return new NextResponse('Not Found', { status: 404 })
+    }
+
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      })
+
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId)
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        })
+      }
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: {
+        id: params.chapterId,
+      },
+    })
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    })
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      })
+    }
+
+    return NextResponse.json(deletedChapter)
+  } catch (error) {
+    console.log('[CHAPTER_ID_DELETE]', error)
+    return new NextResponse('Internal Error', { status: 500 })
+  }
+}
